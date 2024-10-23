be15f53f7147359ae80d1b4271591ba231acaea5
Heena Bhesaniya <heena.bhesaniya@i-link.co.in>
2024-10-23 18:53:05 -0400

    update context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Registration from './pages/Registration'
 import Login from './pages/Login'
 import axios from 'axios'
 import {Toaster} from 'react-hot-toast'
-import { UserContextProvider } from '../context/userContext'
+import { UserContextProvider } from './context/userContext'
 
 axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.withCredentials = true;
