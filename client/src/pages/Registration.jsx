a967c78295eb2e7b17bf759312887ab771be7dcb
Heena Bhesaniya <heena.bhesaniya@i-link.co.in>
2024-11-01 17:17:01 -0400

    chnages for design

--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -23,68 +23,135 @@ const navigate = useNavigate();
 
   return (
     <div className="registration-container">
-    <div className="registration-box">
-      <h2>Register</h2>
-      <form onSubmit={registerUser}>
-        <div className="input-group">
-          <label htmlFor="firstName">First Name</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={data.firstName}
-            onChange={(e) => setData({...data, firstName:e.target.value})}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={data.lastName}
-            onChange={(e) => setData({...data, lastName:e.target.value})}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={data.email}
-            onChange={(e) => setData({...data, email:e.target.value})}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={data.password}
-            onChange={(e) => setData({...data, password:e.target.value})}
-            required
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={data.confirmPassword}
-            onChange={(e) => setData({...data, confirmPassword:e.target.value})}
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+    <div className="image-section">
+    </div>
+    <div className="form-section">
+      <div className="registration-box">
+        <h2>Register</h2>
+        <form  onSubmit={registerUser}>
+          <div className="input-group">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={data.firstName}
+              onChange={(e) => setData({...data, firstName:e.target.value})}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={data.lastName}
+              onChange={(e) => setData({...data, lastName:e.target.value})}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={data.email}
+              onChange={(e) => setData({...data, email:e.target.value})}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={data.password}
+              onChange={(e) => setData({...data, password:e.target.value})}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={data.confirmPassword}
+              onChange={(e) => setData({...data, confirmPassword:e.target.value})}
+              required
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
     </div>
   </div>
+  //   <div className="registration-container">
+  //   <div className="registration-box">
+  //     <h2>Register</h2>
+  //     <form onSubmit={registerUser}>
+  //       <div className="input-group">
+  //         <label htmlFor="firstName">First Name</label>
+  //         <input
+  //           type="text"
+  //           id="firstName"
+  //           name="firstName"
+  //           value={data.firstName}
+  //           onChange={(e) => setData({...data, firstName:e.target.value})}
+  //           required
+  //         />
+  //       </div>
+  //       <div className="input-group">
+  //         <label htmlFor="lastName">Last Name</label>
+  //         <input
+  //           type="text"
+  //           id="lastName"
+  //           name="lastName"
+  //           value={data.lastName}
+  //           onChange={(e) => setData({...data, lastName:e.target.value})}
+  //           required
+  //         />
+  //       </div>
+  //       <div className="input-group">
+  //         <label htmlFor="email">Email</label>
+  //         <input
+  //           type="email"
+  //           id="email"
+  //           name="email"
+  //           value={data.email}
+  //           onChange={(e) => setData({...data, email:e.target.value})}
+  //           required
+  //         />
+  //       </div>
+  //       <div className="input-group">
+  //         <label htmlFor="password">Password</label>
+  //         <input
+  //           type="password"
+  //           id="password"
+  //           name="password"
+  //           value={data.password}
+  //           onChange={(e) => setData({...data, password:e.target.value})}
+  //           required
+  //         />
+  //       </div>
+  //       <div className="input-group">
+  //         <label htmlFor="confirmPassword">Confirm Password</label>
+  //         <input
+  //           type="password"
+  //           id="confirmPassword"
+  //           name="confirmPassword"
+  //           value={data.confirmPassword}
+  //           onChange={(e) => setData({...data, confirmPassword:e.target.value})}
+  //           required
+  //         />
+  //       </div>
+  //       <button type="submit">Register</button>
+  //     </form>
+  //   </div>
+  // </div>
     // <div>
     //     <form  onSubmit={registerUser}>
     //         <h3>Registration</h3>
