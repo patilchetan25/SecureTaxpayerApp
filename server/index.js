5a8dff44b2325fd82651ce444ce4427c0a6a296d
bo <bodrye.kam@gmail.com>
2024-11-15 14:59:32 -0500

    server link changes

--- server/index.js
+++ server/index.js
@@ -17,15 +17,7 @@ const allowedOrigins = [
 
 //middleware
 app.use(cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from your specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: ['https://auto-deploy-helper-dj2lxga3zq-uc.a.run.app', 'http://localhost:8000'],
     credentials: true
   }));
 app.use(express.json());
