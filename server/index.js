039e694eb503d889d4e8b4b5170daee3e7e38726
bo <bodrye.kam@gmail.com>
2024-11-15 13:58:39 -0500

    cors changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose');
@@ -11,24 +12,19 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((error)  => console.log(error))
 
 //middleware
+app.use(cors())
 app.use(express.json());
 app.use(express.static('../client/dist'));
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false}))
 
 app.use('/', require('./routes/authRoutes'))
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 app.use(express.static(path.resolve(__dirname, "dist")))
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"))
 })
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port , () => console.log(`Server is running on port ${port}`));
