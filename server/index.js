858761fe9f303d4633ffaabc14719236ea7164bc
bo <bodrye.kam@gmail.com>
2024-11-15 14:11:08 -0500

    cors fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const path = require("path")
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
-const cors = require('cors')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express();
